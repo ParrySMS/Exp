49f14cde51e992edd4927584d1510ecc6fcf72a8
ParrySMS <yh@szu.me>
2018-03-30 20:51:48 +0800

    Add files via upload

---
+++ ProLang/stuInfo/config/database_info.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: L
+ * Date: 2018-3-20
+ * Time: 10:39
+ */
+
+
+//db
+define("DATABASE_TYPE","mysql");
+define("DATABASE_NAME", "exp");
+define("SERVER", "localhost");
+define("USERNAME", "");
+define("PASSWORD", "");
+define("CHARSET", "utf8");
+define("PORT", 3306);
+define("CHECK_INTERVAL",300);
+define("DB_PREFIX",'pl');
