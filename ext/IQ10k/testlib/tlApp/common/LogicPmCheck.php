cd5368868c5d87b27fca0c279000fdec78f45301
ParrySMS <yh@szu.me>
2018-07-24 23:58:00 +0800

    update

--- ext/IQ10k/testlib/tlApp/common/LogicPmCheck.php
+++ ext/IQ10k/testlib/tlApp/common/LogicPmCheck.php
@@ -14,8 +14,14 @@ class LogicPmCheck extends PmCheck
 
     public function ProInfoRegionCheck(Array $problem_info)
     {
-        //todo: 部分参数的空检查
+        //todo: 部分参数的空检查 字符索引检查
+//        problem_info = compact($problem, $option_num, $options, $answers, $language, $classification, $pro_type, $proSource, $hint);
 
+        if(in_array('',$problem_info)
+        ||in_array(null,$problem_info)){
+            throw new \Exception('null in problem_info',400);
+
+        }
 
         //参数逻辑检查
         $region_lang = json_decode(PM_REGION_LANG_JSON);
