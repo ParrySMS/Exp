7874de70449c18bcafa91b4803a8d770a4af3637
ParrySMS <yh@szu.me>
2018-09-20 22:53:49 +0800

    with bug

---
+++ DS/stack_train_order.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+#define IN 1
+#define OUT 0
+
+void coutMethod(int *arr,int len) {
+	int k;
+	for(k=0; k<len; k++) {
+		if(arr[k]==IN) {
+			cout<<"in"<<endl;
+		} else {
+			cout<<"out"<<endl;
+		}
+	}
+}
+int main () {
+	void coutMethod(int *arr,int len);
+	stack<char> s;
+	int i,j,k,n,len;
+	string str_in,str_out;
+	int  method[20];
+
+	while(cin>>n>>str_in>>str_out) {
+
+		for(i=0,j=0,k=0; i<n; i++) {
+			//i for stack
+			//j for str out
+			//k fot method
+
+			s.push(str_in[i]);
+			method[k]=IN;
+			k++;
+
+			while(str_out[j] == s.top() && j<n && !s.empty()) {
+
+				s.pop();
+				j++;
+				cout<<"pop  ";
+
+				method[k]=OUT;
+				k++;
+				cout <<"out  ";
+			}
+
+		}//end for
+
+		cout<<"end for ";
+		cout<<" i: "<<i;
+		cout<<" j: "<<j;
+		cout<<" k: "<<k;
+
+		if(s.empty() && j==n) { //pop n times
+			cout<<"Yes."<<endl;
+
+		} else {
+			cout<<"No."<<endl;
+		}
+
+		cout<<"method ";
+		coutMethod(method,k);
+
+		//clear
+		while(!s.empty()) {
+			cout<<"clear pop ";
+			s.pop();
+		}
+
+		len = 20;
+		for(k=0; k<len; k++) {
+			method[k]= ' ';
+		}
+		cout<<"clear array ";
+
+
+	}
+
+	return 0;
+}
