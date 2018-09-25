a012f144088de10f8b9af239b00bc0920036ea18
ParrySMS <yh@szu.me>
2018-09-25 17:01:57 +0800

    问题 C: DS队列+堆栈--数制转换

--- DS/queque_num_base.cpp
+++ DS/queque_num_base.cpp
@@ -9,14 +9,39 @@ using namespace std;
 stack <int> whole;//int num
 queue <int>	decimal;
 
+char getSingleChar(int num) {
+	switch(num) {
+		case 10:
+			return 'A';
+			break;
+		case 11:
+			return 'B';
+			break;
+		case 12:
+			return 'C';
+			break;
+		case 13:
+			return 'D';
+			break;
+		case 14:
+			return 'E';
+			break;
+		case 15:
+			return 'F';
+			break;
+		default:
+			return '0' + num ;
+	}
+}
 int main() {
 	int i,t,k,num_w,num_d;
-
+	char getSingleChar(int num) ;
 	int n_int;
 	double n;
 	cin>>t;
 
 	while(t--) {
+
 		cin>>n>>k;
 
 		if(k<=1||k>16) {
@@ -28,53 +53,59 @@ int main() {
 
 		while(1) {
 			num_w = n_int%k;
-				cout<<"push-int: "<<num_w<<endl;
+//			cout<<"push-int: "<<num_w<<endl;
 			whole.push(num_w);
 			n_int = n_int/k;
 
-			if(n_int ==0) {
+			if(n_int == 0) {
 				break;
 			}
 		}
 
 		//decimal num
 		n = (double)(n-(int)n);
-		cout<<"decimal n--> "<<n;
-
+//		cout<<"decimal n--> "<<n;
 		while(1) {
 			n = n*k;
-			num_d = (n>=1)?1:0;
-				cout<<"push-dec: "<<num_d<<endl;
+			num_d = int(n);
+//			cout<<"push-dec: "<<num_d<<endl;
 			decimal.push(num_d);
-			n = (n>=1)?n-1:n;
 
-			if(n == 0.0) {
+			if( n-0.0 < 0.0000000001) {
 				break;
 			}
+
+			n = (double)(n-(int)n);
 		}
 
 		//get result
-		n = 0.0;
+
 		while(!whole.empty()) {
-			n = n*10 + whole.top();
-			//cout<<"int "<<whole.top()<<endl;
+			cout<<getSingleChar(whole.top());
 			whole.pop();
 		}
 
-		i = -1;
+		cout<<".";
+
+		//	need 3 dec
+		while(decimal.size()<3) {
+			decimal.push(0);
+		}
+
+
+		for(i=0; i<3; i++) {
+			if(!decimal.empty()) {
+				cout<<getSingleChar(decimal.front());
+				decimal.pop();
+			}
+		}
+
+		//clear
 		while(!decimal.empty()) {
-			n = n + (pow(10,i)*decimal.front());
-			i--;
-			//cout<<"dec "<<decimal.front()<<endl;
 			decimal.pop();
 		}
 
-		cout<<fixed<<setprecision(3)<<n<<endl;
-
+		cout<<endl;
 	}
-
-
-
-
 	return 0;
 }
