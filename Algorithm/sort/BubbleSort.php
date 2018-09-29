7a56cfbf07b1bb3601238c2356f642b42e15f1a8
ParrySMS <yh@szu.me>
2018-09-29 14:32:01 +0800

    冒泡排序

--- Algorithm/sort/BubbleSort.php
+++ Algorithm/sort/BubbleSort.php
@@ -7,8 +7,14 @@
  */
 
 $ar = [5,8,6,4,12,5,3,1,89,54];
+print_r(json_encode($ar));
+
+echo "<br/>";
+
 bubble($ar);
-print_r($ar);
+print_r(json_encode($ar));
+echo "<br/>";
+
 
 /** 冒泡排序
  * @param array $ar
@@ -20,15 +26,25 @@ function bubble(array & $ar, $len = null)
         $len = sizeof($ar);
     }
 
-    for ($i = 0; $i < $len; $i++) {
-        for ($j = $i + 1; $j < $len; $j++) {
+    for ($i = 0; $i < $len-1; $i++) {
+        for ($j = 0; $j < $len-$i-1; $j++) {
 
-            if ($ar[$i] > $ar[$j]) {//swap
-                $t = $ar[$i];
-                $ar[$i] = $ar[$j];
-                $ar[$j] = $t;
+            if ($ar[$j] > $ar[$j+1]) {//swap
+                echo "swap [$j]--[";
+                echo $j+1;
+                echo ']';
+                
+                echo "<br/>";
+
+                $t = $ar[$j];
+                $ar[$j] = $ar[$j+1];
+                $ar[$j+1] = $t;
             }
+
         }
+        print_r(json_encode($ar));
+        echo "<br/>";
+
     }
 
 }
