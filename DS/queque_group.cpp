16be819b4d1ea241ee1bf9d6a0fd7df539f644e1
ParrySMS <yh@szu.me>
2018-10-09 17:12:59 +0800

    update

--- DS/queque_group.cpp
+++ DS/queque_group.cpp
@@ -11,12 +11,13 @@ using namespace std;
 
 int main() {
 	int i,t,n,value,data;
-	int q1_group_num,q2_group_num,data_group_num;
+	int q_group_num,data_group_num;
 	string op;
 	map <int,int> group;
 	map <int,int>::iterator it;
-	queue <int> q1,q2;
-	bool has;
+	queue <int> q,q2,*p;
+	bool has=true;
+
 
 	cin>>t;
 	while(t--) {
@@ -27,95 +28,64 @@ int main() {
 		}
 	}
 
-
+	p = &q;
 	while(1) {
 		cin>>op;
 		if(op.compare("STOP") == 0) {
-			while(!q2.empty()) {
-				cout<< q2.front()<<endl;
-				q2.pop();
+	
+			while(!q.empty()) {
+				q.pop();
 			}
 			break;
 		}
 
 		if(op.compare("ENQUEUE") == 0) {
 			cin>>data;
-			if(q1.empty()) {
-				q1.push(data);
+			if(p[i].empty()) {
+				p[i].push(data);
 //				cout<<"push first";
 
 			} else { //check same group
 
-				it = group.find(data);
-				data_group_num = it->second;
+				for(i=0; i<t; i++) {
 
-				q2 = q1;//keep q1
+					q_group_num = group[p[i].front()];
+					data_group_num = group[data];
 
-				has = false;
-				while(!q2.empty()) {
-					it = group.find(q2.front());
-					q2_group_num = it->second;
-
-					if(data_group_num == q2_group_num) {
-//						cout<<"has";
-						has = true;
-						//push again make a new queue
+					if(q_group_num == data_group_num) {
+						p[i].push(data);
 						break;
 					}
 
-					q2.pop();
 				}
+			}
 
-				//clear q2;
-				while(!q2.empty()) {
-					q2.pop();
-				}
+		}
 
-				if(has) {//push again make a new queue
+		if(op.compare("DEQUEUE") == 0) {
 
-					it = group.find(q1.front());
-					q1_group_num = it->second;
-					//put q1 in q2
-					while(!q1.empty()) {
-						if(!q2.empty()) {//check q2 last one
-							it = group.find(q2.front());
-							q2_group_num = it->second;
-						}
+			if(has) {
+				cout<<p[0].front();
+				has = false;
+				p[0].pop();
 
-						if(!q2.empty()
-						        && q2_group_num == data_group_num //last one same group
-						        && q1_group_num != data_group_num) {//next one another group
-							//so it is end of last group
-							q2.push(data);
-						} else if(q1.size()==1 && q1_group_num == data_group_num) {//all is same group
-							q2.push(q1.front());
-							q1.pop();
-							q2.push(data);
-						} else {
-							//let other in
-							q2.push(q1.front());
-							q1.pop();
-						}
+			} else {
+				for(i=0; i<t; i++) {
+					if(!p[i].empty()) {
+			//			cout<<" "<<p[i].front();
+						p[i].pop();
+						break;
 					}
-
-				} else { //not has group
-					q1.push(data);
-					q2 = q1;
 				}
-				
-				//store back data
-				q1 = q2;
-
-
 			}
 		}
 
 	}
-
-
+	if(!q.empty()) {
+		q.pop();
+	}
+	delete p;
 	return 0;
-
 }
-
 
 
---
+++ DS/string_kmp.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class myString {
+
+	private:
+		string mainstr;
+
+
+		//	void GetNext(string p,int next[]);
+		int KMPFind(string p,int pos,int next[]);
+
+
+	public:
+		int size;
+		myString();
+		~myString();
+		void SetVal(string sp);
+		int* GetNext(string p);
+		int KMPFindSubstr(string p,int pos);
+		string GetMainstr();
+
+};
+
+string myString::GetMainstr() {
+	return mainstr;
+}
+myString::myString() {
+	size = 0;
+	mainstr ="";
+}
+
+myString::~myString() {
+	size = 0;
+	mainstr = "";
+}
+
+void myString::SetVal(string sp) {
+	mainstr = "";
+	mainstr.assign(sp);
+	size = mainstr.length();
+}
+
+int myString::KMPFindSubstr(string p,int pos) { //find substr loc
+	int i;
+	int len = p.length();
+	int *next;
+	next = GetNext(p);
+
+	for(i=0; i<len; i++) {
+		cout<<next[i]<<' ';
+	}
+
+	cout<<endl;
+
+	int loc = -1;
+	loc = KMPFind(p,pos,next);
+
+	delete []next;
+	
+	loc++;//turn start from 0 
+	
+	cout<<loc<<endl;
+	return loc;
+}
+
+int* myString::GetNext(string p) {
+
+	int len = p.length();
+
+	int *next = new int[len];
+	int j=0,k=-1; //k=next[j]
+
+	next[0]=-1;
+
+
+	while(j<len-1) {
+
+		if(k==-1|| p[j]==p[k]) {
+			next[++j]=++k;
+		} else {
+			k = next[k];
+		}
+	}
+
+	return next;
+
+}
+
+int myString::KMPFind(string p,int pos,int next[]) {
+	int m_len,p_len;
+
+	m_len = GetMainstr().length();
+	p_len = p.length();
+
+	int i = pos,j=0;
+
+	while(i<m_len && j<p_len) {
+		if(j==-1||mainstr[i] == p[j]) {
+			++i;
+			++j;
+		} else {
+			j = next[j];
+		}
+	}
+
+	if(j==p_len) { //i is the end of p
+		return i-p_len;
+	}
+	return -1;
+}
+
+int main() {
+	int i,t;
+	int *next;
+	string mainstr,substr;
+
+
+
+	cin>>t;
+
+	while(t--) {
+		myString *ms = new myString();
+		cin>>mainstr;
+		ms->SetVal(mainstr);
+
+
+		cin>>substr;
+
+		ms->KMPFindSubstr(substr,0);
+
+		ms->~myString();
+	}
+
+	return 1;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
