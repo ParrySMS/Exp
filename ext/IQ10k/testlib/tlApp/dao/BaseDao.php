9be1dc7fc913975faa948fbdc7a3ff21c2842b8c
ParrySMS <yh@szu.me>
2018-11-09 23:51:51 +0800

    Update ext/IQ10k/testlib/tlApp/dao/BaseDao.php

--- ext/IQ10k/testlib/tlApp/dao/BaseDao.php
+++ ext/IQ10k/testlib/tlApp/dao/BaseDao.php
@@ -17,12 +17,18 @@ class BaseDao
     static $T_ACTION = DB_PREFIX.'_action';
     static $T_COMMENT = DB_PREFIX . "_comment_test";
 //    static $T_COMMENT = DB_PREFIX . "_comment_adddiagramspecial";
-    static $T_HINT = DB_PREFIX . "_hint_test";
-  //  static $T_HINT = DB_PREFIX . "_hint_adddiagramspecial";
-  //  static $T_OPTION = DB_PREFIX . "_option_adddiagramspecial";
-	 static $T_OPTION = DB_PREFIX . "_option_test";
-   // static $T_PROBLEM = DB_PREFIX . "_problem_adddiagramspecial";
-    static $T_PROBLEM = DB_PREFIX . "_problem_test";
+
+    //  static $T_HINT = DB_PREFIX . "_hint_adddiagramspecial";
+      static $T_HINT = DB_PREFIX . "_hint_adddiagramspecial_test";
+//    static $T_HINT = DB_PREFIX . "_hint_test";
+
+//    static $T_OPTION = DB_PREFIX . "_option_adddiagramspecial";
+    static $T_OPTION = DB_PREFIX . "_option_adddiagramspecial_test";
+//	 static $T_OPTION = DB_PREFIX . "_option_test";
+
+//    static $T_PROBLEM = DB_PREFIX . "_problem_adddiagramspecial";
+    static $T_PROBLEM = DB_PREFIX . "_problem_adddiagramspecial_test";
+//    static $T_PROBLEM = DB_PREFIX . "_problem_test";
 
     //trans
     static $T_TRANS_PROBLEM = DB_PREFIX . "_problem_cetrans_not_en";
