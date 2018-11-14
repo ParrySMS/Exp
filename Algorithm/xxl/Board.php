bf6000328b9af94f0aa16d481d98403e8ba8d53c
ParrySMS <yh@szu.me>
2018-11-15 06:48:42 +0800

    Update Algorithm/xxl/Board.php

--- Algorithm/xxl/Board.php
+++ Algorithm/xxl/Board.php
@@ -9,14 +9,21 @@
 require "Box.php";
 
 $b = new Board();
-$b->getConnectedBoxes();
+$b->updateBoard($b->getConnectedBoxes());
+
 
 class Board
 {
+    const SCORE_3_BOXES = 1;
+    const SCORE_4_BOXES = 4;
+    const SCORE_5_BOXES = 10;
+
+    const ICON_NULL_ID = -1;
+
     private $board = [];
     private $line = 6;
     private $col = 6;
-    private $icon_max = 4;
+    private $icon_max = 4; //最大icon_id
 
     /**
      * @return array
@@ -27,12 +34,29 @@ class Board
     }
 
     /**
-     * @param array $board
+     *
      */
-    public function setBoard($board)
+    public function setBoard(array $board)
     {
         $this->board = $board;
-    } //最大icon_id
+    }
+
+    /**
+     * @param $x
+     * @param $y
+     * @param $value
+     * @throws Exception
+     */
+    public function setBoardBox($x, $y, $value)
+    {
+        if (sizeof($this->board) <= $x
+            || sizeof($this->board[0]) <= $y) {
+            throw new Exception(__CLASS__ . __FUNCTION__ . ": x or y over board");
+        }
+
+        $this->board[$x][$y] = $value;
+
+    }
 
     /**
      * Board constructor.
@@ -67,12 +91,94 @@ class Board
         for ($x = 0; $x < $this->col; $x++) {
             $col = [];
             for ($y = 0; $y < $this->line; $y++) {
-                $col[] = rand(0, $this->icon_max);
+                $col[] = (int)rand(0, $this->icon_max);
             }
             $this->board[] = $col;
         }
     }
 
+
+    /**  实现数字下落 设置对应box为-1 对应列下坠 每次下落应该展示一次动画 然后循环进行计算消分
+     * @param array $connected_boxes
+     * @throws Exception
+     */
+    public function updateBoard(array $connected_boxes)
+    {
+        $connected = array_merge($connected_boxes[0], $connected_boxes[1]);
+//
+//        echo json_encode($connected);
+//        echo "<br/>";
+//        echo "<br/>";
+
+        //set -1
+        foreach ($connected as $boxs) {
+            foreach ($boxs as $b)
+                $this->setBoardBox($b->x, $b->y, $this::ICON_NULL_ID);
+        }
+
+        //drop
+        foreach ($this->board as & $col) {
+            $drop_step = 0;
+            $height = sizeof($col);
+            for ($i = 0; $i < $height; $i++) {
+
+                if ($col[$i] == $this::ICON_NULL_ID) { //mark upper box drops step
+                    $drop_step++;
+
+                } elseif ($i > 1 && $col[$i - 1] == $this::ICON_NULL_ID) { //drop and swap
+                    $col[$i - $drop_step] = $col[$i];
+                    $col[$i] = $this::ICON_NULL_ID;
+                }
+            }
+
+//            echo json_encode($col);
+//            echo "<br/>";
+//            echo "<br/>";
+        }
+
+    }
+
+    /** 计算对应消除的分数
+     * @param array $connected_boxes
+     * @param int $score
+     * @return int
+     */
+    public function getScore(array $connected_boxes, $score = 0)
+    {
+        if (sizeof($connected_boxes) == 0) {
+            return 0;
+        }
+
+        $connected = array_merge($connected_boxes[0], $connected_boxes[1]);
+
+//        echo json_encode($connected);
+//        echo "<br/>";
+
+        foreach ($connected as $boxs) {
+
+            switch (sizeof($boxs)) {
+                case 3:
+                    $score += $this::SCORE_3_BOXES;
+                    break;
+                case 4:
+                    $score += $this::SCORE_4_BOXES;
+                    break;
+                case 5:
+                    $score += $this::SCORE_5_BOXES;
+                    break;
+            }
+        }
+
+
+        return $score;
+    }
+
+
+    /** 找到相连的部分
+     * @param array $board
+     * @param bool $update 是否更新当前板块
+     * @return array
+     */
     public function getConnectedBoxes(array $board = [], $update = false)
     {
         if (sizeof($board) == 0) {
@@ -82,13 +188,16 @@ class Board
         $line_num = sizeof($board[0]);
         $col_num = sizeof($board);
 
-        echo $line_num;
-        echo "<br/>";
-        echo $col_num;
-        echo "<br/>";
+//        echo $line_num;
+//        echo "<br/>";
+//        echo $col_num;
+//        echo "<br/>";
 
         // $same_lines = []; ////each board ,get the sames line connected-boxes
         $all_same_lines = []; //array of $same_lines
+        for ($i = 0; $i < $line_num; $i++) {
+            $all_same_lines[$i] = [];
+        }
 
         $same_cols = []; //each x ,get the sames cols connected-boxes
         $all_same_cols = []; //array of $same_cols
@@ -117,17 +226,14 @@ class Board
                  * about  $all_same_lines[] --> $same_lines [different key] --> boxes[] --> box
                  */
 
-                // $all_same_lines[$y] is $same_lines
-                if (is_array($all_same_lines) && sizeof($all_same_lines) != $line_num) {
-                    $all_same_lines[$y] = [];
-                }
                 //it is [for] of y ,so need to do sth with $all_same_lines's index, to filled it intermittently
                 //fill box in  $same_line[$key]
                 if ($x != 0 && $board[$x][$y] != $board[$x - 1][$y]) {// new get different icon,changes keys
 
+//                    echo "y:$y,key_x:$key_x. <br/>";
                     // 3 level of ar, all need to check
-                    if (sizeof($all_same_lines)>0
-                        && sizeof($all_same_lines[$y]) > 0
+                    if (sizeof($all_same_lines) >= $y + 1
+                        && sizeof($all_same_lines[$y]) >= $key_x + 1
                         && sizeof($all_same_lines[$y][$key_x]) >= 3) {
 
                         $key_x++; //if save over 3 boxes -> is valid data, key++ ,init
@@ -137,9 +243,14 @@ class Board
                 }
 
                 // x still can connect over 3  or already has parts-boxs add last 3 box
-                if ($x <= $col_num - 3 || sizeof($all_same_lines[$y][$key_x]) > 0) {
+                if ($x <= $col_num - 3) {
 //                $all_same_lines = array of $same_lines
                     //     $same_lines [$key_x]
+                    $all_same_lines[$y][$key_x][] = new Box($x, $y, $board[$x][$y]);
+
+                } else if (sizeof($all_same_lines[$y]) >= $key_x + 1
+                    && sizeof($all_same_lines[$y][$key_x]) > 0) {
+
                     $all_same_lines[$y][$key_x][] = new Box($x, $y, $board[$x][$y]);
                 }
 
@@ -149,16 +260,72 @@ class Board
 
         }//end x for all board
 
+        //clean invaild
+        $line_connected = [];
+        $line_two_connected = [];
+        $col_connected = [];
+        $col_two_connected = [];
 
-        echo json_encode($board);
-        echo "<br/>";
-        echo "<br/>";
-        echo json_encode($all_same_lines);
-        echo "<br/>";
-        echo "<br/>";
-        echo json_encode($all_same_cols);
-        echo "<br/>";
-        echo "<br/>";
+        foreach ($all_same_lines as $units) {
+            if (sizeof($units) == 0) {
+                continue;
+            }
+            foreach ($units as $boxes) {
+                if (sizeof($boxes) < 2) {
+                    continue;
+                } elseif (sizeof($boxes) == 2) { //save for count next step
+                    $line_two_connected[] = $boxes;
+                } else {
+                    $line_connected[] = $boxes;
+                }
+            }
+
+        }
+
+        foreach ($all_same_cols as $units) {
+            if (sizeof($units) == 0) {
+                continue;
+            }
+            foreach ($units as $boxes) {
+                if (sizeof($boxes) < 2) {
+                    continue;
+                } elseif (sizeof($boxes) == 2) { //save for count next step
+                    $col_two_connected[] = $boxes;
+                } else {
+                    $col_connected[] = $boxes;
+                }
+
+            }
+
+
+            $connected_boxes = [$line_connected, $col_connected, $line_two_connected, $col_two_connected];
+
+//        echo json_encode($board);
+//        echo "<br/>";
+//        echo "<br/>";
+//        echo json_encode($all_same_lines);
+//        echo "<br/>";
+//        echo "<br/>";
+//        echo json_encode($all_same_cols);
+//        echo "<br/>";
+//        echo "<br/>";
+//            echo json_encode($connected_boxes[0]);
+//            echo "<br/>";
+//            echo "<br/>";
+//            echo json_encode($connected_boxes[1]);
+//            echo "<br/>";
+//            echo "<br/>";
+
+            unset($all_same_lines);
+            unset($same_cols);
+            unset($all_same_cols);
+            unset($line_connected);
+            unset($col_connected);
+            unset($line_two_connected);
+            unset($col_two_connected);
+
+            return $connected_boxes;
+        }
 
     }
 
