18505a2ee176e6c12bf2d66d539ea1858e8a25c9
ParrySMS <yh@szu.me>
2018-11-20 22:11:21 +0800

    拓扑排序

--- DS/graph_tp.cpp
+++ DS/graph_tp.cpp
@@ -1,37 +1,74 @@
 #include <iostream>
 
-using std;
+using namespace std;
 
-const int MaxLen = 20;
-const int MaxDist = 9999;
+const int MAX = 9999;
 
 int main() {
-	int i,j,n,t;
-	int* mx;
+	int i,j,k,n,t,deg=0,v=-1;
+	int** mx;
+	int* visit;
 	cin>>t;
 	while(t--) {
 		cin>>n;
-		mx = new int[n]();
+		mx = new int*[n];
+		visit = new int[n]();
 
+		//init
 		for(i=0; i<n; i++) {
-			for(j=0; j<n; i++) {
-
+			mx[i] = new int[n]();
+			visit[i] = 0;
+			for(j=0; j<n; j++) {
 				cin>>mx[i][j];
 			}
 		}
 
-		/**TODO 拓扑排序算法：给出有向图邻接矩阵
-		1.逐列扫描矩阵，找出入度为0且编号最小的顶点v
 
-		2.输出v，并标识v已访问
+		for(k=0; k<n; k++) {
+			// 拓扑排序算法：给出有向图邻接矩阵
+			//1.逐列扫描矩阵，找出入度为0且编号最小的顶点v
+			for(i=0,v = MAX; i<n; i++) {
+				deg = 0;
 
-		3.把矩阵第v行全清0
+				if(visit[i] == 1) {
+					continue;
+				}
 
-		重复上述步骤，直到所有顶点输出为止
+				for(j=0; j<n; j++) {
+					deg += mx[j][i];
+				}
 
-		**/
+				if(deg == 0) {
+					v = i<v?i:v;
+				}
+			}
 
+			//	2.输出v，并标识v已访问
+			if(v == MAX) {
+//				cout<<"break ";
+				break; //not found end
+			}
+
+			cout<<v<<" ";
+			visit[v] = 1;
+
+			//	3.把矩阵第v行全清0
+			for(j=0; j<n; j++) {
+				mx[v][j] = 0;
+			}
+			//	重复上述步骤，直到所有顶点输出为止
+		}//for k
+
+		cout<<endl;
+
+	}//while
+
+	int p;
+	for(i=0; i<n; i++) {
+		delete []  mx[i];
 	}
+	delete [] mx;
+	delete [] visit;
 
 	return 0;
 }
