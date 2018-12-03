7439ea91a271c34293c159ed1026713e2dcda3ae
ParrySMS <yh@szu.me>
2018-12-03 21:39:31 +0800

    Update BTB_useless.s

--- COD/BTB_useless.s
+++ COD/BTB_useless.s
@@ -1,6 +1,26 @@
+.text
 ; make BTB useless
 
 ; init var
-daddi $a2,$zero,0 
-daddi $a2,$zero,0 
-daddi $a2,$zero,0 
+	daddi $t0,$zero,10 ;10 is 010,mean 8
+	daddi $a0,$zero,0  ;init a = 0
+	daddi $s0,$zero,0 ;res of a < 0 
+	
+WHILE: 
+	beqz $t0,EXIT ;while t>0
+	slti $s0,$a0,0
+	beqz $s0,SUB     ;a>=0
+	daddi $a0,$a0,1  ;a<0 a++
+	daddi $t0,$t0,-1  ;t--
+	j WHILE
+
+SUB:
+	;a>=0 a--
+	daddi $a0,$a0,-1 
+	daddi $t0,$t0,-1 ;t--
+	j WHILE
+	
+EXIT:
+	halt
+	
+
