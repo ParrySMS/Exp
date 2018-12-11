ae03a6643d7e54c145cfd60a76e0afda995e5c18
ParrySMS <yh@szu.me>
2018-12-11 22:48:55 +0800

    Create hash_link.cpp

---
+++ DS/hash_link.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+const int mod_num = 11;
+int main(){
+	int n,t,i,data;
+	cin>>n;
+	//todo link ar
+	for(i=0;i<n;i++){
+		cin>>data;
+		
+	}
+	
+	return 0;
+}
