80829b2d636a96e5bac10c0263b8ec60bc655f56
ParrySMS <yh@szu.me>
2019-01-21 23:41:56 +0800

    update

---
+++ AlgoExc/ClassicIntro/P7-SwapInt.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: L
+ * Date: 2019-1-21
+ * Time: 23:24
+ *
+ * 输入两个整数 a 和 b
+ * 交换两者的值
+ * 然后输出
+ *
+ * 样例输入 824 16
+ * 样例输出 16 824
+ *
+ *
+ */
+
+fscanf(STDIN,"%d %d",$a,$b);
+想·
+$func1 = function (int $a, int $b){
+    $t = $a;
+    $a = $b;
+    $b = $t;
+
+    echo $a.' '.$b;
+};
+
+$func2 = function (int $a, int $b){ //without more temp var
+    $a = $a+$b; //a = sum
+    $b = $a-$b; // newb <- sum-b = a
+    $a = $a-$b; // a <- sum-newb = sum-a = b
+
+    echo $a.' '.$b;
+};
+
+
+$func3 = function (int $a, int $b){// best one
+    echo $b.' '.$a;
+};
+
+//exc
+$func3($a,$b);
--- ext/mathPro/exercise/rps_mod.php
+++ ext/mathPro/exercise/rps_mod.php
@@ -3,6 +3,8 @@
 
 $cmd=fopen("php://stdin", "r");//打开命令行的输入文件流 用于读入输入的参数
 
+//fscanf(STDIN,"%d %d",$a,$b); 此代码表示，按照指定格式 整数 空格 整数 读取两个整数变量到a和b里，不再需要用fget逐行读取
+
 echo 'A player:'.PHP_EOL; //输出字符串 并且换行
 echo '1 mean rock, 2 mean paper, 3 mean scissors'.PHP_EOL;
 $a = fgets($cmd);//获取A输入的参数
