f322ec699ff50b17ae3e350df35e583ee66deea1
ParrySMS <yh@szu.me>
2019-01-23 00:12:05 +0800

    add f4 -- break continue

--- AlgoExc/ClassicIntro/P17-aabbx2.php
+++ AlgoExc/ClassicIntro/P17-aabbx2.php
@@ -10,55 +10,73 @@
  *
  */
 
-function f1():void
+function f1(): void
 {//先满足aabb  再用开根号来校验完全平方数
 
-    for($a=1;$a<=9;$a++){
-        for($b=0;$b<=9;$b++){
-            $num = $a*1100 + $b*11;
+    for ($a = 1; $a <= 9; $a++) {
+        for ($b = 0; $b <= 9; $b++) {
+            $num = $a * 1100 + $b * 11;
             $root = sqrt($num);
 
             //判断整数  浮点数可能有小数误差
-            if( abs(round($root)-$root)<0.0001){//is int
-                echo $num.PHP_EOL;
+            if (abs(round($root) - $root) < 0.0001) {//is int
+                echo $num . PHP_EOL;
             }
         }
     }
 
 }
 
-function f2():void
+function f2(): void
 {
-    $root=32;
-    while($root*$root<9999){
-        $num = $root*$root;
+    $root = 32;
+    while ($root * $root < 9999) {
+        $num = $root * $root;
         $root++;
 
-        $aa = intval($num/100);
-        $bb = $num%100;
+        $aa = intval($num / 100);
+        $bb = $num % 100;
 
-        if(intval($aa/10) == $aa%10
-        && intval($bb/10) == $bb%10) {
+        if (intval($aa / 10) == $aa % 10
+            && intval($bb / 10) == $bb % 10) {
             echo $num . PHP_EOL;
         }
     }
 }
 
-function f3():void
+function f3(): void
 {//先满足四位完全平方数 再校验aabb
     //32*32=2^10=1024 min四位数, 99*99=100^2-200+1 max四位数
-    for($root=32;$root<99;$root++){
-        $num = $root*$root;
-        $aa = intval($num/100);
-        $bb = $num%100;
+    for ($root = 32; $root < 99; $root++) {
+        $num = $root * $root;
+        $aa = intval($num / 100);
+        $bb = $num % 100;
 
-        if(intval($aa/10) == $aa%10
-        && intval($bb/10) == $bb%10){
-            echo $num.PHP_EOL;
+        if (intval($aa / 10) == $aa % 10
+            && intval($bb / 10) == $bb % 10) {
+            echo $num . PHP_EOL;
         }
     }
 
 }
 
-f2();
+function f4(): void
+{
+    for ($root = 1; ; $root++) {
+        $num = $root * $root;
+
+        if ($num < 1000) continue;
+        if ($num > 9999) break;
+
+        $aa = intval($num / 100);
+        $bb = $num % 100;
+
+        if (intval($aa / 10) == $aa % 10
+            && intval($bb / 10) == $bb % 10) {
+            echo $num . PHP_EOL;
+        }
+    }
+}
+
+f4();
 
