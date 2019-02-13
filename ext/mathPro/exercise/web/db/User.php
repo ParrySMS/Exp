59b9c8442f269b24dd004dcb2c5f7d4f3ffb964c
ParrySMS <yh@szu.me>
2019-02-13 12:52:00 +0800

    update

--- ext/mathPro/exercise/web/db/User.php
+++ ext/mathPro/exercise/web/db/User.php
@@ -10,22 +10,23 @@ class User extends Db
 {
     protected $table = 'user_login';
 
-    public function getAccPwDatas(){
+    public function getAccPwDatas()
+    {
 
 
-        $datas = $this->database->select($this->table,[
+        $datas = $this->database->select($this->table, [
             'account',
             'password'
-        ],[
-            'visible[!]'=>0
+        ], [
+            'visible[!]' => 0
         ]);
 
-        if(!is_array($datas)||sizeof($datas) == 0){
+        if (!is_array($datas) || sizeof($datas) == 0) {
             throw new Exception('DB data error');
         }
 
         $acc_pw_ar = [];
-        foreach ($datas as & $d){
+        foreach ($datas as & $d) {
             $acc_pw_ar[$d['account']] = $d['password'];
         }
 
@@ -33,8 +34,52 @@ class User extends Db
         return $acc_pw_ar;
     }
 
-    public function deleteUser($uid){
+    public function deleteUser($uid)
+    {
         //todo
+    }
+
+    public function insertAcc()
+    {
+        $acc_pw = json_decode(ACC_PW_ARRAY);
+
+        foreach ($acc_pw as $account => $pw) {
+
+            $this->getDatabase()->insert($this->table, [
+                'account' => $account,
+                'password' => $pw,
+                'visible' => VALID_USER
+            ]);
+
+            $id = $this->getDatabase()->id();
+
+            if (empty($id) || !is_numeric($id) || $id <= 0) {
+                throw new Exception(__CLASS__ . '->' . __FUNCTION__ . '():insert id error');
+            }
+
+        }
+
+    }
+
+    public function isEmptyTable(): bool
+    {
+        $has = $this->getDatabase()->has($this->table, [
+            'visible' => 1
+        ]);
+
+        return !$has;
+
+    }
+
+    public function isPw($acc, $pw): bool
+    {
+        $has = $this->getDatabase()->has($this->table, [
+            'account' => $acc,
+            'password' => $pw,
+            'visible' => VALID_USER
+        ]);
+
+        return $has;
     }
 
 
--- ext/mathPro/exercise/web/func/check.php
+++ ext/mathPro/exercise/web/func/check.php
@@ -8,24 +8,44 @@
 
 
 //todo 账号密码检查
-function check($acc,$pw){
-
-
+function check($acc, $pw)
+{
 //    $acc_pw_ar = json_decode(ACC_PW_ARRAY,true);
 
     $user = new User();
+
+    if($user->isEmptyTable()) {
+        $user->insertAcc();
+    }
+
     $acc_pw_ar = $user->getAccPwDatas();
 
-    if(!is_array($acc_pw_ar)){
+    if (!is_array($acc_pw_ar)) {
         throw new Exception('config error: ACC_PW_ARRAY');
     }
 
-    if(!array_key_exists($acc,$acc_pw_ar)){
+    if (!array_key_exists($acc, $acc_pw_ar)) {
         throw new Exception('account error');
     }
 
-    if($pw!=$acc_pw_ar[$acc]){
+    if ($pw != $acc_pw_ar[$acc]) {
         throw new Exception('pw error');
+    }
+
+}
+
+
+
+function checkDBHas($acc, $pw){
+
+    $user = new User();
+
+    if($user->isEmptyTable()) {
+        $user->insertAcc();
+    }
+
+    if(!$user->isPw($acc,$pw)){
+        throw new Exception('account or pw error');
     }
 
 }
