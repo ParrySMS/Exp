276ad7651fc39c722e1dc5ebeef9fa33a185f489
ParrySMS <yh@szu.me>
2019-03-01 16:24:57 +0800

    fix bug: params spelling error

--- ext/IQ10k/testlib/tlApp/dao/BaseDao.php
+++ ext/IQ10k/testlib/tlApp/dao/BaseDao.php
@@ -18,7 +18,7 @@ class BaseDao
     static $T_ACTION = DB_PREFIX.'_action';
 //    static $T_COMMENT = DB_PREFIX . '_comment_test';
 //    static $T_COMMENT = DB_PREFIX . '_comment_adddiagramspecial';
-    static $T_COMMENT = DB_PREFIX . '_commenl'.ET;
+    static $T_COMMENT = DB_PREFIX . '_comment'.ET;
 //    static $T_COMMENT = DB_PREFIX . '_comment_adddiagramspecial_test';
 
 //      static $T_HINT = DB_PREFIX . '_hint_adddiagramspecial';
