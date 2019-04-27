b35425058d56b70073c85950ee686f5569a8608e
ParrySMS <yh@szu.me>
2019-04-27 23:13:45 +0800

    Update check.php

--- ext/IQ10k/TT-Prediction/admin/func/check.php
+++ ext/IQ10k/TT-Prediction/admin/func/check.php
@@ -54,7 +54,28 @@ function allEngS($str){
     return (preg_match("/^[a-z\s]+$/",$str));
 }
 
+/** 判断是否为邮箱
+ * @param $str
+ * @return false|int
+ */
 function isEmail($str){
     $pattern = '/^[a-z0-9]+([._-][a-z0-9]+)*@([0-9a-z]+\.[a-z]{2,14}(\.[a-z]{2})?)$/i';
     return preg_match($pattern,$str);
+}
+
+
+/** 行为记录 请求一次记录 报错一次记录
+ * @param $uid = null
+ * @param null $error_code
+ */
+function action($uid = null, $error_code = null)
+{
+    $http = new Http();
+    $ip = $http->getIP();
+    $agent = $http->getAgent();
+    $uri = $_SERVER['REQUEST_URI'];
+    $method = $_SERVER['REQUEST_METHOD'];
+    // 实现dao类
+    $action = new Action();
+    $action->insert($uid, $ip, $agent, $uri, $method, $error_code);
 }
