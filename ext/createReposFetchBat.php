e220bd0553a80421af86416a451b54a2a002806e
ParrySMS <yh@szu.me>
2019-09-11 18:07:57 +0800

    add branch checkout

--- ext/createReposFetchBat.php
+++ ext/createReposFetchBat.php
@@ -9,14 +9,27 @@
 define('REPOSITORIES_LOCAL_PATH', 'D:/Dev');
 
 /**
- *  It is a default config that the local branch is set-upstream-to origin with same name
- *  eg.
- *     master    --> remotes/origin/master
- *     develop_A --> remotes/origin/develop_A
+ *   It is a default config that the local branch is set-upstream-to origin with same name
+ * eg.
+ *   master    --> remotes/origin/master
+ *   develop_A --> remotes/origin/develop_A
  *
  *   Branch 'A' set up to track remote branch 'A' from 'origin'.
  *   So this bat is able to pull by branch name
+ *
+ *   The define of REPOSITORIES_WITH_ORIGIN_BRANCH is mean that you want to
+ *   keep your REPOSITORIES_WITH_LOCAL_BRANCH following the REPOSITORIES_WITH_ORIGIN_BRANCH
+ * eg.
+ *   [ develop_A --> remotes/origin/develop_A ] ===> keep following [remotes/origin/develop-QA]
+ *
+ *   Branch 'develop_A' want to be the same as origin/develop-QA, both [local/develop_A] and [origin/develop_A] are.
+ *   so the define of these two array should be:
+ *
+ * define('REPOSITORIES_WITH_LOCAL_BRANCH',  ['repos_name' => 'develop_A'];
+ * define('REPOSITORIES_WITH_ORIGIN_BRANCH', ['repos_name' => 'develop-QA'];
+ *
  */
+
 define('REPOSITORIES_WITH_LOCAL_BRANCH', [
     'economic' => 'master',
     'Exp' => 'master',
@@ -57,6 +70,7 @@ foreach (REPOSITORIES_WITH_LOCAL_BRANCH as $repos => $local_branch){
     if($local_branch == REPOSITORIES_WITH_ORIGIN_BRANCH[$repos]){
         $cmd .= 'git pull'.PHP_EOL;
     }else{
+        $cmd .= 'git checkout '.$local_branch.PHP_EOL;
         $cmd .= 'git merge origin/'.REPOSITORIES_WITH_ORIGIN_BRANCH[$repos].PHP_EOL;
         $cmd .= 'git push'.PHP_EOL;
     }
